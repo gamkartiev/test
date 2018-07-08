740a4e459374079a2821fe932d363f194a478d89
gamkartiev <gamkartiev@yandex.ru>
2018-07-08 23:32:14 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,41 @@
 <html>
-<head>
-	<title>Test</title>
-</head>
+<title>Тестовое задание доделанное</title>
+<link rel="stylesheet" href="main.css">
 <body>
+<a href="index.php" class="link"> На главную </a>
+	<?php 
+error_reporting(E_ALL);
+$ip_local=$_SERVER['REMOTE_ADDR'];
+$ip_local = sprintf('%u', ip2long($ip_local));  //Переводим ip-адресс в целое число
+
+require_once ("database.php");
+$link = db_connect();
+$users = users($link);  //Функция для вывода всех данных при загрузки админки
+
+
+if(isset ($_POST['login']) and ($_POST['password'])){	//Если значения login и password установлены, то...
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	$user = user($link, $login, $password);				//...Вызываем функцию, которая вытащит соответствующий аккаунт
+
+	if(!empty($user) and in_array($ip_local, $user)){   //Если переменная не пуста и ip с компьютера сходится с указанным в базе, то...
+		include "admin.php";							//..подключаем админку с таблицей инфы про юзеров,а если нет и...
+	} elseif(!empty($user)){							// ..если переменная не пуста, а ip не совпадает, то выводим сообщение о заперете
+		echo "Вход с вашего ip-адреса запрещен!";
+	} else{
+		echo "Неправильная комбинация логина-пароля";  //Если переменная юзер пуста, то выводим о неправильном логине или пароле
+	}
+}else{													//Если значения логин и пароль не установлены, то выводим форму для авторизации
+	?>
+
 	<form action='index.php' method='post'>
-		<input name='login'>
-		<input name='password' type='password'>
+		<input name='login'> <br>
+		<input name='password' type='password'> <br>
 		<input type='submit' value='Отправить'>
 	</form>
 
-
-
-<?php 
-error_reporting(E_ALL);
-$ip_local=$_SERVER['REMOTE_ADDR'];
-$ip_local = ip2long($ip_local);  //Переводим ip-адресс в целое число
-
-require_once ("database.php");
-
-$link = db_connect();
-$users = users($link);
-
-//Если форма авторизации отправлена...
-	if ( !empty($_REQUEST['password']) and !empty($_REQUEST['login']) ) {
-		//Пишем логин и пароль из формы в переменные (для удобства работы):
-		$login = $_REQUEST['login'];
-		$password = $_REQUEST['password'];
-
-		/*
-			Формируем и отсылаем SQL запрос:
-			ВЫБРАТЬ ИЗ таблицы_users ГДЕ поле_логин = $login И поле_пароль = $password.
-		*/
-		$query = 'SELECT*FROM users WHERE login="'.$login.'" AND password="'.$password.'"';
-		$result = mysqli_query($link, $query); //ответ базы запишем в переменную $result
-
-		$user = mysqli_fetch_assoc($result); //преобразуем ответ из БД в нормальный массив PHP
-		//$ip = $user['ip1'];
-
-
-		//Если база данных вернула не пустой ответ - значит пара логин-пароль правильная
-		if (!empty($user) and in_array($ip_local, $user)) {
-			echo "$user[login] Вы успешно авторизировались";
-			include "admin.php";	
-			//Пользователь прошел авторизацию, выполним какой-то код.
-		} elseif (!empty($user) and $ip_local!=$user['ip1']||$user['ip2']||$user['ip3']) //Пользователь все верно ввел, но не запрещенный ip-адрес
-		 {
-			echo $ip_local;
-			echo "Вход для Вашего адреса не разрешен";
-		}
-		else {
-			//Пользователь неверно ввел логин или пароль, выполним какой-то код.
-			echo "Неверная комбинация логина-пароля";
-		}
-	}
-
- ?>
+	<?php 
+}
+	 ?>
 </body>
 </html>
